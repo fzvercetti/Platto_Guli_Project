911d86409c23f09129b55257bf5304e2a5f498f0
fzvercetti <felipezamorabaldazo@gmail.com>
2026-05-30 23:33:53 -0600

    cambios en el main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../screens/menu_page.dart';
 
-// 1. Definimos el notificador global aquí mismo (o impórtalo si lo tienes en otro archivo)
 final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.light);
 
 void main() {
@@ -13,7 +12,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // 2. Usamos ValueListenableBuilder para escuchar cambios de tema
     return ValueListenableBuilder<ThemeMode>(
       valueListenable: themeNotifier,
       builder: (_, ThemeMode currentMode, __) {
@@ -21,7 +19,6 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'Platto Menú',
 
-          // --- CONFIGURACIÓN DE TEMAS ---
           theme: ThemeData.light(), // Tema claro
           darkTheme: ThemeData.dark(), // Tema oscuro
           themeMode: currentMode, // El tema que se aplica actualmente
